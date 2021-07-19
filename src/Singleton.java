ba4f52a800c961fab1d91e02665a1efe6382142c
saber aoufi <saber.aoufi.@gmail.com>
2021-07-19 13:20:08 +0100

    singleton pattern

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,4 +1,15 @@
 public class Singleton {
- 
-    
+ private static Singleton Singleton =new Singleton();
+    private Singleton()
+    {}
+
+    public static Singleton getInstance()
+    {
+        return Singleton;
+    }
+
+    public void show()
+    {
+        System.out.println("Singleton pattern");
+    }
 }
